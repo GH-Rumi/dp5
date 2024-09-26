7e4c067aec3b4bab41feba4172c3b54110022a1e
rumi <user@LAPTOP-0IU79HM9git config --global user.name rumigit onfig --get-all user.namegit onfig --get-all user.nameGH-Rumigit onfig --get-all user.name-- get-all GH-Rumi!git config --global user.email user@LAPTOP-0IU79HM9>
2024-09-26 08:45:29 +0700

    revisi

--- Praktikum05/PemilihanAngka11.java
+++ Praktikum05/PemilihanAngka11.java
@@ -8,14 +8,9 @@ public class PemilihanAngka11 {
         Scanner sc = new Scanner(System.in);
         System.out.println("Masukkan sebuah angka: ");
         int angka = sc.nextInt();
-        if (angka % 2 == 0)
-        {
-            System.out.println("Angka " + angka + " termasuk bilangan genap");
-        }
-        else
-        {
-            System.out.println("Angka " + angka + " termasuk bilangan ganjil");
-        }
+        String hasil;
+        hasil = (angka % 2) == 0 ? " bilangan genap" : " bilangan ganjil";
+        System.out.println(angka + " adalah" + hasil);
 
     }
 }
